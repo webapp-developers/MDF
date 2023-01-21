fb7b450f06b2add42e1e728f2d69981edd77cab2
shivam sisodia <shivamsisodia8656816@gmail.com>
2023-01-21 19:38:26 +0530

    added contact us and product page

--- js/scripts.js
+++ js/scripts.js
@@ -49,9 +49,40 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 $('.single-item').slick({
     dots: true,
-    autoplay: true,
+    // autoplay: true,
     infinite: true,
-    speed: 300,
+    speed: 500,
     slidesToShow: 1,
-    adaptiveHeight: true
+    // adaptiveHeight: true
 });
+
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
